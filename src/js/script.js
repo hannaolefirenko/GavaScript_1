438382b82f0f40641636fb5694df26bc9d0dad8c
Anna <olefirenko000@gmail.com>
2024-02-07 18:42:06 +0100

    types_of_data

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,95 @@
-let text = "Pryvit";
-const numbers = 123456789;
+// "use strict";
 
-alert(text);
-alert(numbers);
-window.alert("our first js lesson");
+// let userName ='John';
+// let userNumber = 25;
+
+// userNumber = 24;
+
+// console.log(userName);
+// console.log(userNumber);
+
+
+
+
+// let num = 3;
+// let element;
+// element = 'Body';
+// alert(num);
+// window.alert(element);
+
+// a = 15;
+// console.log(a);
+
+// const carBodyLength = 3000;
+// const carBodyWidth = 5000;
+
+
+// console.log('Розміри авто fiat ${carBodyLength} на ${carBodyWidth}');
+
+// request (server)
+// data
+// response
+
+// camelCase - змінні створюємо
+// snake_case - гілки створюємо
+//UPPER_SNAKE_CASE - підписуємо те, що не можна змінювати
+
+// let RED_COLOR = '#F00;
+
+// const _apiBase ='';
+
+// let firstNum = 4.4;
+// // console.log(number + 5);
+
+// console.log(typeof(firstNum));
+// console.log(typeof(Hello)); //string
+
+// let und;
+// console.log(typeof(und));
+
+// let text = "Lorem"; //string - в кавичках завжди, двойні або одинарні, різні
+
+// Логічні типи даних (true/false) - boolean
+// let man = true;
+// let man2 = false;
+
+// console.log(typeof(man));
+
+// console.log(4/2);
+
+// const woman = { // object в фігурних дужках
+//     name: "Anna",
+//     age: 25,
+//     isMarried: true
+// };
+
+// console.log(woman);
+// console.log(woman.isMarried);
+
+// const woman1 = { // array
+//     0: "Anna",
+//     1: 25,
+//     2: true
+// };
+
+// // Maccиви
+// let arr = ["Theo", 1.5, false];
+// let fruits = ["Apple", "Orange", "Слива"];
+
+// console.log(arr[1]);
+// console.log(fruits[1]);
+
+
+// завдання
+let nameStore = "Kramnytsia";
+const storeDescription = {
+    budget: 10000,
+    employees: ["Santa", "Maria", "Lucia"],
+    products: {
+        milk: 20,
+        bread: 30
+    },
+    isOpen: true
+};
+
+console.log(storeDescription);
